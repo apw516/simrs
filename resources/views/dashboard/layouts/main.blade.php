5cb2a3ed28370f6cccea2027af1272517db9a809
apw516 <redhawk5665@gmail.com>
2022-04-20 08:45:15 +0700

    logo

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -9,7 +9,7 @@
     <title>
         {{ $title }}
     </title>
-
+    <link href='{{ asset('public/img/logo_rs.png')}}' rel='shortcut icon'>
     <!-- Google Font: Source Sans Pro -->
     {{-- <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> --}}
