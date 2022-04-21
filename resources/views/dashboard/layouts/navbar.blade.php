f54133a462f0a1064e6b6d6fcd7ea87db6e8fb65
apw516 <redhawk5665@gmail.com>
2022-04-21 11:13:56 +0700

    a

--- resources/views/dashboard/layouts/navbar.blade.php
+++ resources/views/dashboard/layouts/navbar.blade.php
@@ -5,7 +5,7 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a class="nav-link text-bold">{{ longdate_indo(date('Y-m-d')) }}</a>
+            {{-- <a class="nav-link text-bold">{{ longdate_indo(date('Y-m-d')) }}</a> --}}
         </li>
     </ul>
 </nav>
