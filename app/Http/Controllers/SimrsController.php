19d3ba0801aa72db48ea276134f8c41e393cc215
apw516 <redhawk5665@gmail.com>
2022-04-29 11:36:40 +0700

    insert tracer

--- app/Http/Controllers/SimrsController.php
+++ app/Http/Controllers/SimrsController.php
@@ -964,8 +964,8 @@ class SimrsController extends Controller
             'id_status_tracer' => 1,
             'cek_tracer' => 'N'
         ];
-        //insert ke tracer
-        // tracer::create($data_tracer);
+        // insert ke tracer
+        tracer::create($data_tracer);
         $data = [
             'kode' => 200,
             'message' => 'sukses',
