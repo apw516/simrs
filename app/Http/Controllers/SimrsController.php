021edfd7d1a242224b8675357749a91df0c655ae
apw516 <redhawk5665@gmail.com>
2024-07-02 12:11:12 +0700

    test

--- app/Http/Controllers/SimrsController.php
+++ app/Http/Controllers/SimrsController.php
@@ -771,9 +771,28 @@ class SimrsController extends Controller
             'f2' => $r2
         ]);
     }
+    function get_client_ip() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
     public function Simpansep(Request $request)
     {
         //antrian
+        dd($this->get_client_ip());
         $mw = new antrianmarwan();
         $day = $request->tglsep;
         $today = strtoupper(Carbon::parse($day)->dayName);
