2191d839329a76872010e68c9760b5dcad6bd1c8
apw516 <redhawk5665@gmail.com>
2025-04-11 09:55:23 +0700

    UPDATE

--- app/Http/Controllers/ErmController.php
+++ app/Http/Controllers/ErmController.php
@@ -605,7 +605,8 @@ class ErmController extends Controller
         $unitk = $kunjungan[0]->kode_unit;
         $rm = $kunjungan[0]->no_rm;
         $kunjunganKronis = DB::select('select * from ts_kunjungan where no_rm = ? and kode_unit = ? and catatan = ?', [$rm, $unitk, 'KRONIS']);
-        $rujukan = $kunjungan[0]->no_rujukan;
+        // $rujukan = $kunjungan[0]->no_rujukan;
+        $rujukan = 'AB'
         $cekrujukan1 = strlen($rujukan);
         if ($cekrujukan1 > 5) {
             $cekrujukan2 = substr($rujukan, 0, 8);
