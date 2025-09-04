0803841050e6fddf96d8e2f71e85888b007adf94
apw516 <redhawk5665@gmail.com>
2025-09-04 10:30:24 +0700

    update

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -553,6 +553,20 @@
                               <p>Data User</p>
                           </a>
                       </li>
+                      <li class="nav-item ">
+                          <a href="{{ route('indexresumemedisrajal') }}"
+                              class="nav-link @if ($sidebar == 'indexresumemedisrajal') active @endif">
+                              <i class="bi bi-filetype-doc nav-icon"></i>
+                              <p>Resume Medis Rajal</p>
+                          </a>
+                      </li>
+                      <li class="nav-item ">
+                          <a href="{{ route('indexresumemedisranap') }}"
+                              class="nav-link @if ($sidebar == 'indexresumemedisranap') active @endif">
+                              <i class="bi bi-filetype-doc nav-icon"></i>
+                              <p>Resume Medis Rawat Inap</p>
+                          </a>
+                      </li>
                   @endif
                   @if (auth()->user()->hak_akses == 6)
                       <li class="nav-header">FARMASI</li>
@@ -566,7 +580,8 @@
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a href="{{ route('index_cek_pasien_kronis') }}" class="nav-link @if ($sidebar == 'index_cek_pasien_kronis') active @endif">
+                          <a href="{{ route('index_cek_pasien_kronis') }}"
+                              class="nav-link @if ($sidebar == 'index_cek_pasien_kronis') active @endif">
                               <i class="nav-icon fas fa-th"></i>
                               <p>
                                   Cek Pasien Kronis
@@ -592,9 +607,12 @@
                           </a>
                       </li>
                   @endif
-                  @if (auth()->user()->nama == 'agyl' || auth()->user()->hak_akses == '1' || auth()->user()->hak_akses == '101' || auth()->user()->hak_akses == '68')
+                  @if (auth()->user()->nama == 'agyl' ||
+                          auth()->user()->hak_akses == '1' ||
+                          auth()->user()->hak_akses == '101' ||
+                          auth()->user()->hak_akses == '68')
                       <li class="nav-header">REKAMEDIS</li>
-                       <li hidden class="nav-item">
+                      <li hidden class="nav-item">
                           <a href="{{ route('dataermrajal') }}"
                               class="nav-link @if ($title == 'SIMRS - BERKAS ERM RAWAT JALAN') active @endif">
                               <i class="nav-icon fas fa-th"></i>
@@ -603,7 +621,7 @@
                               </p>
                           </a>
                       </li>
-                      <li  class="nav-item @if ($sidebar == 'berkas_erm') menu-open @endif">
+                      <li class="nav-item @if ($sidebar == 'berkas_erm') menu-open @endif">
                           <a href="#" class="nav-link">
                               <i class="nav-icon fas fa-table"></i>
                               <p>
