54846fb613bf0f6a6f74a25e04ed32a6ea9e8c20
apw516 <redhawk5665@gmail.com>
2025-10-04 09:33:06 +0700

    update

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -23,7 +23,7 @@ class PdfController extends Controller
         // Storage::put('public/qr/' . $qrImageName, $qr);
         $now = $this->get_now();
         $qrcode = base64_encode(QrCode::format('svg')->size(200)->errorCorrection('H')->generate('string'));
-        $customPaper = array(0,0,660,520);
+        $customPaper = array(0,0,660,520,'landscape');
         $pdf = Pdf::loadView('pdf.cetakansep', compact([
             'sep',
             'qrcode',
