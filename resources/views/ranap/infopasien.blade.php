592826b278ce9d8cd208c16f843a03f1c049e3c7
apw516 <redhawk5665@gmail.com>
2026-01-02 11:19:38 +0700

    update

--- resources/views/ranap/infopasien.blade.php
+++ resources/views/ranap/infopasien.blade.php
@@ -68,6 +68,13 @@
                                 <option value="2023">2023</option>
                                 <option value="2024">2024</option>
                                 <option value="2025">2025</option>
+                                <option value="2026">2026</option>
+                                <option value="2027">2027</option>
+                                <option value="2028">2028</option>
+                                <option value="2029">2029</option>
+                                <option value="2030">2030</option>
+                                <option value="2031">2031</option>
+                                <option value="2032">2032</option>
                             </select>
                         </div>
                         <button type="" @if ($datapasien[0]->no_Bpjs == '') disabled @endif
