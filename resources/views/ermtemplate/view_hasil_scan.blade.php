7a64a82b0feb6443f89b5ec9f6f7a44169826640
apw516 <redhawk5665@gmail.com>
2026-01-30 10:09:35 +0700

    ad

--- resources/views/ermtemplate/view_hasil_scan.blade.php
+++ resources/views/ermtemplate/view_hasil_scan.blade.php
@@ -1,7 +1,7 @@
 @foreach ($cek as $c )
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 <div class="card">
-    <div class="card-header"></div>
+    <div class="card-header bg-light text-bold">@if($c->jenisberkas == 1) Rawat Inap @elseif($c->jenisberkas == 2)Rawat Jalan @else Penunjang @endif</div>
     <div class="card-body">
         <iframe src ="{{ $c->fileurl }}" width="1000px" height="600px"></iframe>
     </div>
