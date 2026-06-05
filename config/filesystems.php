cd84d0813a9bb77f7a230ee21ea28ead894f68ae
apw516 <redhawk5665@gmail.com>
2026-06-05 12:12:07 +0700

    ad

--- config/filesystems.php
+++ config/filesystems.php
@@ -28,6 +28,7 @@ return [
     |
     */
 
+    
     'disks' => [
 
         'local' => [
@@ -37,47 +38,47 @@ return [
         ],
         'shared2' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\resume_medis_rawat_jalan_no_ttd',
+            'root' => '\\\\192.168.2.14\\erm\\resume_medis_rawat_jalan_no_ttd',
         ],
         'shared' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\resume_medis_rawat_jalan',
+            'root' => '\\\\192.168.2.14\\erm\\resume_medis_rawat_jalan',
         ],
         'SEP' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\sep',
+            'root' => '\\\\192.168.2.14\\erm\\sep',
         ],
         'LAB_1' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\hasil_lab_1',
+            'root' => '\\\\192.168.2.14\\erm\\hasil_lab_1',
         ],
         'LAB_2' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\hasil_lab_2',
+            'root' => '\\\\192.168.2.14\\erm\\hasil_lab_2',
         ],
         'LAB_3' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\hasil_lab_3',
+            'root' => '\\\\192.168.2.14\\erm\\hasil_lab_3',
         ],
         'LAB_4' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\hasil_lab_4',
+            'root' => '\\\\192.168.2.14\\erm\\hasil_lab_4',
         ],
         'LAB_5' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\hasil_lab_5',
+            'root' => '\\\\192.168.2.14\\erm\\hasil_lab_5',
         ],
         'RAD' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\expertisi_radiologi',
+            'root' => '\\\\192.168.2.14\\erm\\expertisi_radiologi',
         ],
         'FAR' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\resepfarmasi',
+            'root' => '\\\\192.168.2.14\\erm\\resepfarmasi',
         ],
         'MER' => [
             'driver' => 'local',
-            'root' => '\\\\192.168.30.98\\erm\\merger_resume_rajal',
+            'root' => '\\\\192.168.2.14\\erm\\merger_resume_rajal',
         ],
         'public' => [
             'driver' => 'local',
