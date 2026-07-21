78356917db9c18c67aa0470501c14e73f6e46b75
apw516 <redhawk5665@gmail.com>
2026-07-21 08:35:23 +0700

    apdet

--- app/Http/Controllers/ErmController.php
+++ app/Http/Controllers/ErmController.php
@@ -779,7 +779,7 @@ class ErmController extends Controller
                     $unit = '1006'; //1
                 }
                 $pasienpoli = DB::select('SELECT IFNULL(d.nomorantrean, TIME(a.`tgl_masuk`)) AS antrian,d.`nomorantrean`,a.kode_kunjungan,fc_nama_unit1(a.kode_unit) as nama_unit,a.no_rm,fc_nama_px(a.no_rm) as nama_pasien,a.ref_kunjungan,b.namapemeriksa,c.nama_dokter,a.`kode_kunjungan`,a.`tgl_masuk`,fc_NAMA_PENJAMIN2(a.`kode_penjamin`) AS nama_penjamin,a.`kode_penjamin`,b.`id` AS id_pemeriksaan_perawat,c.id AS id_pemeriksaan_dokter,b.status as status_asskep,c.status as status_assdok,date(e.tgl_lahir) as tgl_lahir FROM ts_kunjungan a LEFT OUTER JOIN erm_hasil_assesmen_keperawatan_rajal b ON a.kode_kunjungan = b.kode_kunjungan LEFT OUTER JOIN assesmen_dokters c ON b.`kode_kunjungan` = c.id_kunjungan LEFT OUTER JOIN jkn_antrian d ON a.`kode_kunjungan` = d.`kode_kunjungan`
-                INNER JOIN mt_pasien e ON b.no_rm = e.no_rm
+                INNER JOIN mt_pasien e ON a.no_rm = e.no_rm
                  WHERE a.`kode_unit` = ? AND DATE(a.tgl_masuk) BETWEEN ? AND ? AND status_kunjungan != ? ORDER BY a.kode_kunjungan ASC', [
                     $unit,
                     $request->tgl_awal,
